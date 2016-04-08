51449c0dff84c0dc8d71ac4e4e477df545c7ff74
Sean Harrison <sah@blackearth.us>
2016-04-07 22:00:52 -0500

    config.json => setup.json

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ path = os.path.dirname(os.path.abspath(__file__))
 with open(os.path.join(path, 'README.md'), encoding='utf-8') as f:
     read_me = f.read()
 
-with open(os.path.join(path, 'config.json'), encoding='utf-8') as f:
+with open(os.path.join(path, 'setup.json'), encoding='utf-8') as f:
     config = json.loads(f.read())
 
 setup(
