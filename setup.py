06ac5021bce3d0d82aef9fad1cc2bdd6b5edf2ac
Sean Harrison <sah@blackearth.us>
2016-04-11 18:19:24 -0500

    setup.json included in setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,29 @@
+config = {
+  "name": "",
+  "version": "0.1",
+  "description": "",
+  "url": "",
+  "author": "Sean Harrison",
+  "author_email": "sah@blackearthgroup.com",
+  "license": "LGPL 3.0",
+  "classifiers": [
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+    "Programming Language :: Python :: 3"
+  ],
+  "entry_points": {},
+  "install_requires": ["bl"],
+  "extras_require": {
+    "dev": [],
+    "test": []
+  },
+  "package_data": {
+    "": []
+    },
+  "data_files": [],
+  "scripts": []
+}
 
 import os, json
 from setuptools import setup, find_packages
@@ -7,9 +33,6 @@ path = os.path.dirname(os.path.abspath(__file__))
 
 with open(os.path.join(path, 'README.rst'), encoding='utf-8') as f:
     read_me = f.read()
-
-with open(os.path.join(path, 'setup.json'), encoding='utf-8') as f:
-    config = json.loads(f.read())
 
 setup(
     long_description=read_me,
